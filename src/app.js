2909179a52a680c3a65c9b3dcd6ee028dcfdcfd1
Betsabet <betsabet.c@gmail.com>
2021-05-17 19:03:58 -0500

    Updated html

--- src/app.js
+++ src/app.js
@@ -29,13 +29,26 @@ function displayTemperature(response) {
     humidityElement.innerHTML = (response.data.main.humidity);
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000); 
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png``);
     iconElement.setAttribute("alt", response.data.weather[0].description);
 } 
 
-let apiKey = "576745a04c3376d5329a519bd2c46d25";
-let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Ciudad de Mexico&appid=576745a04c3376d5329a519bd2c46d25&units=metric";
+function search(city) {
+    let apiKey = "576745a04c3376d5329a519bd2c46d25";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
 
+
+
+
+
+let form = document.querySelector("#search-form");
+form addEventListener("submit", handleSubmit);
 

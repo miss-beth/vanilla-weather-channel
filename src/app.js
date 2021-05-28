9ae31d653c85dd537e82c97bab22dbbb3e3c65d5
Betsabet <betsabet.c@gmail.com>
2021-05-28 17:39:47 -0500

    Added forecast days in a row

--- src/app.js
+++ src/app.js
@@ -13,6 +13,35 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 } 
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+    days.forEach(function(day) {
+         forecastHTML = forecastHTML + 
+                        `
+                        <div class="col-2">
+                            <div class="weather-forecast-date">
+                                ${day}
+                            </div>
+                            <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" width="42" />
+                            <div class="weather-forecast-temperatures">
+                                <span class="weather-forecast-temperature-max">
+                                    18°
+                                </span>
+                                <span class="weather-forecast-temperature-min">
+                                    12°
+                                </span>
+                            </div>
+                        </div>`; 
+    })
+   
+    forecastHTML = forecastHTML + `</div>`;        
+    forecastElement.innerHTML = forecastHTML;
+
+}
+
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -76,3 +105,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Los Angeles");
+displayForecast();

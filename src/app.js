fc4d8547a022934f9f8e055836587372b5c3cd0d
Betsabet <betsabet.c@gmail.com>
2021-06-02 13:22:07 -0500

    Added the forecast functions

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 } 
 
-function displayForecast() {
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
@@ -42,6 +42,12 @@ function displayForecast() {
 
 }
 
+function getForecast(coordinates) {
+    let apiKey = "576745a04c3376d5329a519bd2c46d25";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -62,6 +68,8 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000); 
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 } 
 
 function search(city) {
@@ -105,4 +113,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Los Angeles");
-displayForecast();
